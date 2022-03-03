6d6f7945d6b4aabd1f1ccd616f5f4316d2a137e3
will <wbrycejohnson031086@gmail.com>
2022-03-02 19:11:01 -0500

    finish part 2

--- controllers/flights.js
+++ controllers/flights.js
@@ -43,7 +43,8 @@ function createTicket(req, res) {
   Flight.findById(req.params.id, function(err, flight) {
     flight.tickets.push(req.body)
     flight.save(function(err){
-      res.redirect(`flights/${flights._id}`)
+      res.redirect(`/flights/${flight._id}`)
+      console.log(flight.ticket)
     })
   })
 }
